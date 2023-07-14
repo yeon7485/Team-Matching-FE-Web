42552c37bcd9dcc4a82be01665d4927ffd2749d2
seiyeon <wsx4379@naver.com>
2023-07-15 00:18:40 +0900

    design: footer 레이아웃 수정
    footer 레이아웃 수정
    - footer가 contents를 가리는 현상 수정

--- team-matching-web/src/App.js
+++ team-matching-web/src/App.js
@@ -1,22 +1,27 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
+import './App.css';
 
 function App() {
   const location = useLocation();
   if (location.pathname === '/join' || location.pathname === '/login')
     return (
-      <>
-        <Outlet />
+      <div className='wrapper'>
+        <div className='contentWrapper'>
+          <Outlet />
+        </div>
         <Footer />
-      </>
+      </div>
     );
   return (
-    <>
+    <div className='wrapper'>
       <Navbar />
-      <Outlet />
+      <div className='contentWrapper'>
+        <Outlet />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
