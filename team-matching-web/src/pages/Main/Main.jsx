91e2795ef2b35f2bfa3f2d7334394235144dc67b
seriokim <atomyoun@gmail.com>
2023-07-22 02:34:10 +0900

    api: login function
    api: login function

--- team-matching-web/src/pages/Main/Main.jsx
+++ team-matching-web/src/pages/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import styles from './Main.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Main() {
   return (
