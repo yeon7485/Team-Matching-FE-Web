014ad4302565c9e1f8ed41f2144b7e09be5e6c77
seiyeon <wsx4379@naver.com>
2023-12-09 20:02:39 +0900

    #20 - mypage 신청 중인 팀 조회 기능 구현
    - 신청 중인 팀 조회 기능 구현
    - teamItem 추가

---
+++ team-matching-web/src/components/TeamItem/TeamItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './TeamItem.module.css';
+import useCategory from '../../hooks/useCategory';
+
+export default function TeamItem({
+  teamData: { name, category, hashtag, capacity, total },
+  onClick,
+  handleCancel,
+  type,
+}) {
+  return (
+    <li className={styles.teamLi}>
+      <div className={styles.item}>{useCategory(category)}</div>
+      <div className={styles.item} onClick={onClick}>
+        {name}
+      </div>
+      <div className={styles.item}>#{hashtag}</div>
+      {type !== 'act' && (
+        <div className={styles.cancel} onClick={handleCancel}>
+          취소
+        </div>
+      )}
+      {type === 'act' && (
+        <div className={styles.item}>{`${total}/${capacity}`}</div>
+      )}
+    </li>
+  );
+}
