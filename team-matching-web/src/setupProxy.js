f28f472ec2894b95efdaa97e4afd1c119afbf2f4
seiyeon <wsx4379@naver.com>
2024-01-16 23:54:29 +0900

    refactor: 환경변수 이름 수정
    환경변수 이름 수정

--- team-matching-web/src/setupProxy.js
+++ team-matching-web/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.SERVER_URL,
+      target: process.env.REACT_APP_SERVER_URL,
       changeOrigin: true,
     })
   );
